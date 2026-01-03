e745ca51ba5c65d6169a5b960d499751bb85fc9b
sawan jha <jhasawan02@gmail.com>
2026-01-03 14:58:53 +0530

    while watchinng day03 class

--- Day2/app.js
+++ Day2/app.js
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React!"
+import { jsx } from "react/jsx-runtime";
+
+const jxsheading = <h1 className="heading">Namaste React with JSX!!</h1>;
+const numbber = 1000;
+
+const HeadingComponent = () => (
+  <div id="container">
+    <h1>{2 + 5}</h1>
+    <h1> This is a functional component</h1>
+  </div>
 );
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {key:1}, "This is Namaste React "),
-    React.createElement("h1", {key:2}, "I am a h2 tag"),
-  ])
-);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);
+
+root.render(<HeadingComponent />);
