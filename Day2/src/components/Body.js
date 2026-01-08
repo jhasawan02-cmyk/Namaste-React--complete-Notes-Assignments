72531028b1ba21bf3aa57e7c660d461edd09fa5d
sawan jha <jhasawan02@gmail.com>
2026-01-09 00:11:50 +0530

    day06-Fetch API

--- Day2/src/components/Body.js
+++ Day2/src/components/Body.js
@@ -1,17 +1,30 @@
 import RestaurantCard from "./RestaurantCard";
-import reslist from "../utils/mockData";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 const Body = () => {
-  const [listOfResttraunts,setlistOfResttraunts]= useState(reslist);
+  const [listOfResttraunts,setlistOfResttraunts]= useState([]);
+
+  useEffect(() => {
+    FetchData();
+  },[]);
+
+  const FetchData = async () =>{
+    const data =  await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+
+    const json = await data.json();
+    console.log(json);
+    setlistOfResttraunts(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+  }
+
+
   return (
     <div className="body">
       <div className="search">
         <input type="text" placeholder="Search for restaurants..."></input>
         <button className="filter-btn" onClick = {()=>{
           const filteredList = listOfResttraunts.filter(
-            (res) => res.info.avgRating > 4.6 
+            (res) => res.info.avgRating > 4.5
           );
           setlistOfResttraunts (filteredList);
         }}
